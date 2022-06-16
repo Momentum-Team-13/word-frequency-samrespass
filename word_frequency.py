87053c03c60047e4758374bdb713c6eee7225625
Sam Respass <samrespass@gmail.com>
2022-06-15 23:27:03 -0400

    fucking dummy

--- word_frequency.py
+++ word_frequency.py
@@ -1,13 +1,28 @@
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
+STOP_PUNC = [
+    ',', '.', '\'', ',','?',
+]
+
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    print (f"Your file is: {file}")
+    with open(file) as open_file:
+        read_file = open_file.read()
+        for stop in STOP_WORDS:
+            string = read_file.replace(stop, '').lower()
+            for puncs in STOP_PUNC:
+                no_punc = string.replace(puncs, '')
+    make_dic = str.split(no_punc)
+    for lyrics in sorted(set(make_dic)):
+        print(lyrics, make_dic.count(lyrics))
 
 
 if __name__ == "__main__":
